990e254ae261adc6eb04688c4e6ae1c761221f45
egbas <emmanuelonaivi@gmail.com>
2022-10-14 13:36:18 -0700

    base16 and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* autor: favvy
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int favvy;
+
+	for (favvy = 48; favvy < 58; favvy++)
+	{
+		putchar(favvy);
+
+		if (favvy != 57)
+
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
