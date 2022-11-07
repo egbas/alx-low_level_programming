19baf3f3d85d7f6fd780c03bcdf0d34a12180fab
egbas <emmanuelonaivi@gmail.com>
2022-11-07 09:38:02 -0800

    changing name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* @argc: count the number of CL args
+* @argv: an array of pointers character
+*
+* Return: return and int
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc); 
+	printf("argv[0] = %s\n", argv[0]);
+
+	return (0);
+
+}
